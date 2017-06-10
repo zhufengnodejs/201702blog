1018bfd6809c422f929437d0e4f47db6dc19e1b4
zhufengnodejs <zhufengnodejs@126.com>
2017-06-10 11:29:32 +0800

    实现导航功能

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 //当客户端以GET方法访问/的时候，执行对应的回调函数
 router.get('/',function(req,res){
-  res.send('首页');
+  res.render('index',{title:'首页'});
 });
 
 module.exports = router;
