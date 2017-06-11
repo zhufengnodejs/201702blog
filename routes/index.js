6179b3177e5da955b26ea4530052695f0f9884a8
zhufengnodejs <zhufengnodejs@126.com>
2017-06-11 13:03:13 +0800

    发表文章的功能

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 let express = require('express');
 let router = express.Router();
-
+let Article = require('../model').Article;
 //当客户端以GET方法访问/的时候，执行对应的回调函数
 router.get('/',function(req,res){
-  res.render('index',{title:'首页'});
+  //populate填充，用来把外键字段从字符串转成对应的文档对象
+  Article.find({}).populate('user').exec(function(err,articles){
+      console.log(articles);
+      res.render('index',{title:'首页',articles});
+  });
 });
 
 module.exports = router;
