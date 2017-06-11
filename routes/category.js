709c92c3d2fd1ae50231fa0fa543910bc55fe1dc
zhufengnodejs <zhufengnodejs@126.com>
2017-06-11 11:19:42 +0800

    使用中间件来判断是否登录

--- routes/category.js
+++ routes/category.js
@@ -1,6 +1,9 @@
 let express = require('express');
 let router = express.Router();
 router.get('/add',function(req,res){
-    res.send('增加分类');
+    res.render('category/add',{title:'增加分类'});
+});
+router.get('/list',function(req,res){
+    res.render('category/list',{title:'分类列表'});
 });
 module.exports = router;
