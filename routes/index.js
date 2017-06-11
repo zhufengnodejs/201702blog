73ca7721ed154f3fd1f0a6341cd135bcb0de1064
zhufengnodejs <zhufengnodejs@126.com>
2017-06-11 16:47:38 +0800

    查询

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,14 @@ let router = express.Router();
 let Article = require('../model').Article;
 //当客户端以GET方法访问/的时候，执行对应的回调函数
 router.get('/',function(req,res){
+  let {keyword} = req.query;
+  let query = {};
+  if(keyword){
+      let reg = new RegExp(keyword);
+      query['$or'] = [{title:reg},{content:reg}];
+  }
   //populate填充，用来把外键字段从字符串转成对应的文档对象
-  Article.find({}).populate('user').exec(function(err,articles){
+  Article.find(query).populate('user').exec(function(err,articles){
       res.render('index',{title:'首页',articles});
   });
 });
